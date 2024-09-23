6473fe18a3d551265f826102b735674edf22cd41
GwilPlay <119343712+GwilPlay@users.noreply.github.com>
2024-09-23 15:28:18 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,9 @@
+x = input('Введите число: ')
+y = input('Введите число: ')
+x1 = input('Введите число: ')
+y1 = input('Введите число: ')
+
+if x == x1 is not y == y1:
+    print('Yes')
+else:
+    print('No')
